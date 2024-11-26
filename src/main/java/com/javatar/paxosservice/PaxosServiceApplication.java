39785b7a5dc84c2b23c23752fe5504ff63593201
esmaeilsadeghijob <esmaeilsadeghijob@gmail.com>
2024-11-26 12:05:13 -0800

    first commit

---
+++ src/main/java/com/javatar/paxosservice/PaxosServiceApplication.java
@@ -0,0 +1,13 @@
+package com.javatar.paxosservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaxosServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaxosServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javatar/paxosservice/PaxosServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatar.paxosservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaxosServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
